8eb321e824e439254043b7e471e96ca70267bca8
Alex Tunbridge <alextunbridge67@gmail.com>
2019-04-28 01:39:55 -0600

    Added post json format.

--- services/posts.js
+++ services/posts.js
@@ -1,4 +1,7 @@
 const Post = require('../models/Post')
+const User = require('../models/User')
+const Category = require('../models/Category')
+const moment = require('moment')
 
 const getPosts = (requestQuery) => new Promise((resolve, reject) => {
   if(requestQuery.limit){
@@ -39,13 +42,25 @@ const getPosts = (requestQuery) => new Promise((resolve, reject) => {
 const getPost = (whereQuery) => new Promise((resolve, reject) => {
   Post.findOne(whereQuery, '-comments').then((post) => {
     if(post) {
-      const response = {
-        status: 200,
-        data: {
-          post
+      formatPost(post).then((post) => {
+        const response = {
+          status: 200,
+          data: {
+            post
+          }
         }
-      }
-      resolve(response)
+        resolve(response)
+      }).catch((err) => {
+        console.log(err)
+        const response = {
+          status: 500,
+          data: {
+            error: '500 Internal server error.',
+            data: err
+          }
+        }
+        reject(response)
+      })
     } else {
       const response = {
         status: 404,
@@ -163,6 +178,52 @@ const removePost = (postSlug) => new Promise((resolve, reject) => {
     reject(response)
   })
 })
+
+const formatPost = (postData) => new Promise((resolve, reject) => {
+  //Get names of categories instead of id's
+  const categories = []
+  postData.categories.forEach((catId) => {
+    Category.findById(catId).then((category) => {
+      if(category) categories.push(category.name)
+    }).catch((err) => {
+      console.log(err)
+    })
+  })
+  //Pull in author's information.
+  console.log(categories)
+  Promise.all(categories).then((categories) => {
+    console.log(categories)
+    User.findById(postData.author).then((user) => {
+      const response = {
+        slug: postData.slug,
+        title: postData.title,
+        body: postData.body,
+        categories: categories,
+        author: {
+          email: user.email,
+          name: user.name
+        },
+        createdAt: moment(postData.createdAt).fromNow(),
+        updatedAt: moment(postData.createdAt).fromNow()
+      }
+      resolve(response)
+    }).catch((err) => {
+      const response = error500(err)
+      reject(response)
+    })
+  })
+})
+
+const error500 = (error = '') => {
+  console.log(error)
+  return {
+    status: 500,
+    data: {
+      error: '500 Internal server error.',
+      data: error
+    }
+  }
+}
 
 module.exports = {
   getPosts,
