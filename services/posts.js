e0cc8babfa100977784af15f17b724686a46e6a1
Alex Tunbridge <alextunbridge67@gmail.com>
2019-04-30 01:39:11 -0600

    ...

--- services/posts.js
+++ services/posts.js
@@ -191,7 +191,7 @@ const formatPost = async (postData) => {
     const categoryIds = postData.categories.map(id => mongoose.Types.ObjectId(id));
     
     // Get names of categories instead of id's
-    const categories = await Category.find().where().in(categoryIds).exec();
+    const categories = await Category.find({_id: {"$in": categoryIds}}).exec();
 
     // Get the author of the post
     const author = await User.findById(postData.author).exec();
